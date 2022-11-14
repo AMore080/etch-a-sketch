2a38407f857b86a8efca16652539b810910d74c8
Alex <alexmore080@gmail.com>
2022-11-14 13:37:58 -0600

    functioning etch

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const pixelValue = document.getElementById('pixelValue');
 const slider = document.getElementById('sizeRange');
 const etchGrid = document.getElementById('etchGrid')
+const color = document.getElementById('favcolor')
 
-
+// Set up for values
 slider.oninput = function() {
     etchGrid.innerHTML = ""
     pixelValue.innerHTML = `${this.value} x ${this.value}`;
@@ -18,16 +19,26 @@ slider.oninput = function() {
     }
 }
 
+// Determines Grid Size
 grid = (gridsize) => {
     etchGrid.style.gridTemplateRows = `repeat(${gridsize},1fr)`
     etchGrid.style.gridTemplateColumns = `repeat(${gridsize},1fr)`
 
     console.log(gridsize)
-    for(let i = 0; i < gridsize; i++){
+    for(let i = 0; i < (gridsize * gridsize); i++){
         const newDiv = document.createElement('div');
         newDiv.classList.add('etchBoxes')
         etchGrid.appendChild(newDiv)
     }
+
+    const etchBoxes = document.querySelectorAll('.etchBoxes');
+    etchBoxes.forEach(box => {
+        box.addEventListener('mousemove', () => {
+            box.style.backgroundColor = color.value
+            box.classList.add('color-box');
+        })
+    })
+
 }
 
 
